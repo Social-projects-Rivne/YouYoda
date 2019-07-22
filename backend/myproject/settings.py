aec3662a94fb6967006f71e43faf472b076e345b
marina1408 <marina1408@ukr.net>
2019-07-22 17:07:27 +0300

    Modifieded settings.py

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MYSQL_DATABASE',
+        'USER': 'MYSQL_USER',
+        'PASSWORD': 'MYSQL_PASSWORD',
+        'HOST': 'mariadb',   
+        'PORT': '3465',
     }
 }
 
