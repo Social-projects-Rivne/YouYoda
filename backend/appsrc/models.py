86a0c83d4c7320b7a3a5c59d8add7ef7f24e836f
marina1408 <marina1408@ukr.net>
2019-07-22 13:02:03 +0300

    Created models

--- backend/appsrc/models.py
+++ backend/appsrc/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+	id = 
+    name = models.UUIDField(primary_key=True)
+    
+class Events(models.Model):
+    pass
+
+class Courses(models.Model):
+    pass
+
+class PDP(models.Model):
+    pass
+
+class Roles(models.Model):
+    pass
+
+class UserStatus(models.Model):
+    pass
+
+class StatusHistory(models.Model):
+    pass
+
+class EventsSubscribers(models.Model):
+    pass
+
+class TrainersCertificates(models.Model):
+    pass
+
+class CoursesSubscribers(models.Model):
+    pass
+
+class Achievements(models.Model):
+    pass
+
+class UserRegistrationTokens(models.Model):
+    pass
+
+class Categories(models.Model):
+    pass
+
