71bf57202b4531311e688a694210d480795f7b5b
Helen P <evanescingh@gmail.com>
2019-07-29 23:30:03 +0300

    subtask 14 - UI form for registration without including

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './index.css';
 import './style/home.css';
+import './style/forms.css';
 import './style/media.css';
 
 
