04d195d4dc15a6dfcd6ffca694d48522fe1c9037
Helen P <evanescingh@gmail.com>
2019-07-31 01:08:20 +0300

    subtask 5 - unification templates for modal forms

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import {
-  Container, Col, Form,
+  Col, Form,
   FormGroup, Label, Input,
-  Button, Row
+  Button, Row, Modal
 } from 'reactstrap';
 import axios from 'axios';
 import FacebookLogo from '../imgnotignor/Facebook(1).png';
@@ -15,10 +15,18 @@ class LoginForm extends Component {
     	super(props);
 
     	this.state={
+			modal: false,
     		email:'',
   			password:''
-  		};
-  	}
+		  };
+		this.toggle = this.toggle.bind(this);
+	}
+	
+	toggle() {
+        this.setState(prevState => ({
+          modal: !prevState.modal
+        }));
+    }
 
   	handleClick(event){
 		 var apiBaseUrl = "http://localhost:8000/api/";
@@ -45,84 +53,93 @@ class LoginForm extends Component {
 		 .catch(function (error) {
 		 console.log(error);
 		 });
-		 }
+	}
     render() {
         return (
-		  <Container>
+		  <div>
+	<Button color="danger" onClick={this.toggle}>Click here</Button>
+			  <Modal id="login-form" isOpen={this.state.modal} toggle={this.toggle} className="wild">
+			  <div className="modal-body">
+			  	<button onClick={this.toggle} type="button" className="close">
+					<span aria-hidden="true">&times;</span>
+				</button>
 			<Row>
-			  <Col md="6">
-			    <section className="Login">
-			      <Form>
-				    <Row>
+			  <Col md="7" className="Login">
+			      <Form className="form-horizontal">
+				    <Row className="m-0">
 				      <Col>
-				        <h2>Log In<br></br><br></br></h2>
+						<h3 className="modal-title mb-3">Log In</h3>
 					    <FormGroup>
-						  <Label>Email</Label>
+						  <Label for="email" className="mb-1">Email</Label>
 						  <Input
 						type="email"
 						name="email"
-						id="exampleEmail"
+						id="email"
 						placeholder="myemail@email.com"
+						required
 						onChange = {(event,newValue) => this.setState({email:newValue})}
 						  />
 					    </FormGroup>
 					    <FormGroup>
-						  <Label for="examplePassword">Password</Label>
+						  <Label for="password" className="mb-1">Password</Label>
 						  <Input
 						type="password"
 						name="password"
-						id="examplePassword"
+						id="password"
 						placeholder="********"
+						required
 						onChange = {(event,newValue) => this.setState({password:newValue})}
 						/>
 					    </FormGroup>	
 				      </Col>
 				    </Row>
-				    <Row>
+				    <Row className="m-0">
 					  <Col>
 					    <FormGroup check>
 						  <Input type="checkbox" name="check" id="exampleCheck"/>
 						  <Label for="exampleCheck" check>Remember me</Label>
 					    </FormGroup>
 					  </Col>
-					  <Col></Col>
-					  <Col>
+					  <Col className="text-right">
 					    <a href="dog.html">Fogot password?</a>
 					  </Col>
 				    </Row>
-				    <Row>
-				      <Col>
-				        <br></br>
-				        <Button className="SignInButton" onClick={(event) => this.handleClick(event)}>Sign in</Button>
+				    <Row className="m-0">
+				      <Col className="mt-4 text-right">
+				        <Button type="submit" className="btn-yellow btn btn-warning" onClick={(event) => this.handleClick(event)}>Sign in</Button>
 				      </Col>
 				    </Row>
 				  </Form>
-				  <br></br><br></br>
-				    <Row>
+				    <Row className="m-0 mt-4 mb-4">
 				      <Col xs="3">
 				        <p>Sign in with:</p>
 				      </Col>
-				      <Col xs="1">
+				      <Col xs="2">
 				        <a href="#"><img src={FacebookLogo} width="40" alt="FacebookLogo"/></a>
 				      </Col>
-				      <Col xs="1"></Col>
-				      <Col xs="1">
+				      <Col xs="2">
 				        <a href="#"><img src={GoogleLogo} width="40" alt="GoogleLogo"/></a>
 				      </Col>
-				     </Row>
-			    </section>
+				    </Row>
 			  </Col>
-			  <Col md="6">
-			    <section className="SignUp">
-			      <h1>Hello,<br></br>Dear Friend</h1>
-			      <br></br>
-			      <p className="SignUpText">Blah blah blah</p>
-			      <p className="SignUpInvite">If you don’t have an account yet</p>
-			      <Button className="SignUpButton">Sign up</Button>
-			    </section>
+			  <Col md="5" className="SignUp">
+			  	<Row className="container h-auto mx-0 mb-4">
+					<h1 className="modal-title text-white custom-title">Hello, <span>Dear Friend!</span></h1>
+                </Row>
+				<Row className="container mx-0 mb-4">
+					<h2 className="text-white">blah blah blah</h2>
+				</Row>
+				<Row className="container h-auto">
+					<div className="col-sm-12 form-group">
+						<p className="text-title text-white d-block pb-2">If you don’t have an account yet</p>
+						<Button color="gray" type="button" className="btn-grey btn">Sign up</Button>
+					</div>
+                </Row>      
 			  </Col> 
 			</Row>
-		  </Container>
+		</div>
+		</Modal>
+		</div>
         );
     }
 }
