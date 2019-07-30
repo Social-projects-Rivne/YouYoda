21fa6d0dc3bd585923fbaaefacd30d0c64017ef1
Helen P <evanescingh@gmail.com>
2019-07-30 22:32:16 +0300

    subtask 14 - reactstrap modal window skeleton

--- frontend/src/Components/Registration.jsx
+++ frontend/src/Components/Registration.jsx
@@ -1,30 +1,45 @@
 import React from 'react';
+import { Button, Modal, Row } from 'reactstrap';
 
 class Registration extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          modal: false
+        };
+    
+        this.toggle = this.toggle.bind(this);
+    }
+    
+    toggle() {
+        this.setState(prevState => ({
+          modal: !prevState.modal
+        }));
+    }
   render () {
     return (
-      <div className="modal fade show" id="registration-form" tabIndex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog wild" role="document">
-        <div class="modal-content">
+    <div>
+        <Button color="danger" onClick={this.toggle}>Click here</Button>
+        <Modal id="registration-form" isOpen={this.state.modal} toggle={this.toggle} className="wild">
             <div className="modal-body">
 
-            <div className="row">
+            <Row>
                 <div className="col-md-5 left-wrap">
-                    <div class="row container h-50 mx-0 mb-4">
+                    <div className="row container h-50 mx-0 mb-4">
                         <h1 className="modal-title text-white custom-title">Hello, <span>Dear Friend!</span></h1>
                     </div>
-                    <div class="row container h-auto">
-                        <div class="col-sm-12 form-group">
-                            <span class="text-title text-white d-block pb-2">If you already have an account</span>
+                    <div className="row container h-auto">
+                        <div className="col-sm-12 form-group">
+                            <span className="text-title text-white d-block pb-2">If you already have an account</span>
                             <button type="button" className="btn-grey btn">Log in</button>
                         </div>
                     </div>
                 </div>
                 <div className="col-md-offset-3 col-md-7 right-wrap">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <button onClick={this.toggle} type="button" className="close">
                         <span aria-hidden="true">&times;</span>
                     </button>
-                    <h3 class="modal-title">Register</h3>
+                    <h3 className="modal-title">Register</h3>
                     <form className="form-horizontal">
                         <div className="form-group">
                             <label htmlFor="email" className="mb-1">Email</label>
@@ -60,12 +75,11 @@ class Registration extends React.Component{
                         </div>
                     </form>
                 </div>
-            </div>
+            </Row>
 
             </div>
-        </div>
-        </div>
-      </div>
+      </Modal>
+    </div>
       )
   }
 }
