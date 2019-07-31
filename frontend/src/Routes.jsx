49c98768aa709f3451fcb7f98909bef6203b0663
Roman <romanrusin06@gmail.com>
2019-07-31 16:52:23 +0300

    edited Routes.jsx

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Home from './Pages/Home';
 import {Route, BrowserRouter as Router} from 'react-router-dom';
+import EditPageProfile from "./Pages/EditPageProfile";
 
 
 export default function Routes() {
@@ -9,6 +10,7 @@ export default function Routes() {
     <Router>
     <div>
         <Route path='/' component={Home}/>
+        <Route path='/editprofile' component={EditPageProfile}/>
     </div>
     </Router>
   );
