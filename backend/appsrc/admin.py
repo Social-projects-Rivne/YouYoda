bb9501286645fee94bad21f62650482dd646300a
Helen P <evanescingh@gmail.com>
2019-08-10 19:25:22 +0300

    task 43 subtask 120 - to implement UI page for admin dashboard

--- backend/appsrc/admin.py
+++ backend/appsrc/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from appsrc.models.user import User
+from appsrc.models.roles import Roles
+from appsrc.models.userstatuses import UserStatuses
+from appsrc.models.statushistory import StatusHistory
+from appsrc.models.userregistrationtokens import UserRegistrationTokens
+from appsrc.models.achievements import Achievements
+from appsrc.models.categories import Categories
+from appsrc.models.courses import Courses
+from appsrc.models.coursessubscribers import CoursesSubscribers
+from appsrc.models.events import Events
+from appsrc.models.eventssubscribers import EventsSubscribers
+from appsrc.models.trainercertificates import TrainerCertificates
+
+admin.site.register(User)
+admin.site.register(Roles)
+admin.site.register(UserStatuses)
+admin.site.register(StatusHistory)
+admin.site.register(UserRegistrationTokens)
+admin.site.register(Achievements)
+admin.site.register(Categories)
+admin.site.register(Courses)
+admin.site.register(CoursesSubscribers)
+admin.site.register(Events)
+admin.site.register(EventsSubscribers)
+admin.site.register(TrainerCertificates)
--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'static/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
