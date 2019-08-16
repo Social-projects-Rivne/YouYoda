5e72b96fb9b8a322ff8284dc0323c5b5a363de47
OleksandrHavrylchyk <sashagavrulchuk@gmail.com>
2019-08-16 15:42:33 +0300

    Fixed view and serializer with new db models

--- backend/appsrc/serializers/user_to_trainer_serializer.py
+++ backend/appsrc/serializers/user_to_trainer_serializer.py
@@ -2,7 +2,12 @@ from rest_framework import serializers
 
 from ..models.user import User
 
-class UserSerializer(serializers.ModelSerializer):
+class UserToTrainerSerializer(serializers.ModelSerializer):
+	"""Takes or updates data from the YouYodaUser model for change user role.
+
+    Converts it to JSON format for transmission via the API.
+
+    """
 
 	class Meta:
 
@@ -11,3 +16,10 @@ class UserSerializer(serializers.ModelSerializer):
 		 fields = (
 		 	'is_trainer',
 		 	)
+   def update(self, instance, validated_data):
+        """Update user role"""
+
+        instance.is_trainer = validated_data.get('is_trainer', instance.is_trainer)
+
+        instance.save()
+        return instance
--- backend/appsrc/views/user_to_trainer.py
+++ backend/appsrc/views/user_to_trainer.py
@@ -1,22 +1,24 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from ..models.user import User
-from ..serializers.user_serializer import UserSerializer
+
+from ..models import YouYodaUser
+from ..serializers.user_to_trainer_serializer import UserToTrainerSerializer
 
 
 class UserToTrainer(APIView):
+	"""Takes data from UserToTrainerSerializer for change user role."""
 
-	def get(self, request):
-		users = User.objects.all();
-		serializer = UserSerializer(users, many=True)
+	permission_classes = [permissions.IsAuthenticated,]
 
-		return Response(serializer.data)
-
-	def put(self, request):
-		serializer = UserSerializer(data=request.data)
+	def patch(self, request):
+		"""Receives and updates user role"""
+		user= get_object_or_404(YouYodaUser, email=request.user)
+		serializer = UserToTrainerSerializer(user)
 		if serializer.is_valid():
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
