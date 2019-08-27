c6d10595dae130b717bd03fa593fde38f428a5a5
marina1408 <marina1408@ukr.net>
2019-08-27 14:17:40 +0300

    Added pagination and event's card

--- frontend/src/Components/EventsList.jsx
+++ frontend/src/Components/EventsList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, Row, Col, InputGroup, InputGroupText, InputGroupAddon, Input, Button } from 'reactstrap';
+import { Container, Row, Col, InputGroup, InputGroupText, InputGroupAddon, Input, Button, Card, CardTitle, CardText, CardHeader, CardFooter, CardBody, Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
@@ -9,10 +9,10 @@ export default class EventsList extends React.Component{
         return(
         	<div>
         	  <Container>
-        	   <br />
+             <div className="search-input-group">
         	    <Row>
-        	      <Col xs="12" md="8">
-        	       <InputGroup>
+        	      <Col xs="12" md={{ size: 7, offset: 1 }}>
+        	       <InputGroup className="search-input">
                      <InputGroupAddon addonType="prepend">
                        <InputGroupText>
                        <FontAwesomeIcon icon="search"/>
@@ -20,9 +20,6 @@ export default class EventsList extends React.Component{
                      </InputGroupAddon>
                      <Input placeholder="Search by trainer, events, tag" />
                        <InputGroupAddon addonType="append">
-                         <InputGroupText> <a href="#"></a>
-                         <FontAwesomeIcon icon={['fas', 'map-marker-alt']}/>
-                         </InputGroupText>
                          <Input type="select" name="select" id="exampleSelect">
                            <option value="" selected>Category</option>
                            <option>1</option>
@@ -31,12 +28,107 @@ export default class EventsList extends React.Component{
                            <option>4</option>
                            <option>5</option>
                          </Input>
-                       </InputGroupAddon>
-                       <Button color="warning" className="btn-search-events">Search</Button>                   
+                         <InputGroupText> 
+                         <FontAwesomeIcon icon={['fas', 'map-marker-alt']}/>
+                         </InputGroupText>
+                       </InputGroupAddon>              
                      </InputGroup>
-                   </Col>
+                     </Col>
+                     <Col xs="8" md="4">
+                     <Button color="warning" className="btn-search-events">
+                     <span className="btn-search-events-title">SEARCH</span>
+                     </Button>  
+                 </Col>
         	     </Row>	
-        	    <br />
+               </div>
+        	   
+
+              <Row>
+              <Col>
+              <h1 className="title-events">Events</h1>
+              </Col>
+              </Row>
+
+               <Row>
+      <Col xs="12" sm="6" md="4">
+        <Card className="event-card">
+        <div className="event-header">
+        <CardHeader>
+          <Row>
+          <Col>1 September</Col>
+          <Col style={{textAlign: "right"}}>18:00</Col>
+          </Row>
+        </CardHeader>
+        </div>
+        <div className="event-body">
+        <CardBody>
+          <CardTitle>Solo concert of Oleh Vynyk</CardTitle>
+          <CardText>
+          <p>$ 2</p>
+          <div>
+          <Row>
+          <Col md="1">
+          <FontAwesomeIcon icon={['fas', 'map-marker-alt']}/>
+          </Col>
+          <Col md="6"><p>Rivne, Stadium Avangard</p></Col></Row>
+          </div>
+          </CardText>
+         
+
+        </CardBody>
+        </div>
+         <CardFooter>
+         <img width="100%" src={require('../img/static/event.png')} alt="Card image event" />
+         </CardFooter>
+      </Card>
+      </Col>
+     
+      
+
+    </Row> 
+
+    <div className="pagination"> 
+    <Pagination size="lg" aria-label="Page navigation example">
+      <PaginationItem disabled>
+          <PaginationLink first href="#" />
+        </PaginationItem>
+        <PaginationItem disabled>
+          <PaginationLink previous href="#" />
+        </PaginationItem>
+        <PaginationItem active>
+          <PaginationLink href="#">
+            1
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">
+            3
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">
+            4
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">
+            5
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink next href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink last href="#" />
+        </PaginationItem>
+      </Pagination>
+      </div>
+
         	  </Container>
         	</div>       
         	);
