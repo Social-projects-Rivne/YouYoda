48db893149ae88e1f628a9c0837140fa03163a42
marina1408 <marina1408@ukr.net>
2019-08-30 09:45:34 +0300

    Added library moment.js, edited modal window for view detail information about event

--- frontend/src/Components/HomeEvent.jsx
+++ frontend/src/Components/HomeEvent.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
 import { Container, Row, Button, Col, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import Slider from "react-slick";
 
+import moment from 'moment';
+
 import { axiosGet } from '../api/axiosGet';
+import { isAuthenticated } from '../utils';
+
 
 
 export default class HomeEvent extends React.Component{
@@ -15,39 +18,48 @@ export default class HomeEvent extends React.Component{
             eventsList: [ {
    "name": "Event-5",
    "description": "Perfect for people who never swam",
-   "date": "2019-08-28T10:26:37.438766+03:00",
+   "date": "2019-08-29T16:58:02.979209+03:00",
    "cover_url": "",
-   "location": "Rivne, Ukraine"
+   "location": "Rivne, Ukraine",
+   "categories": "sport",
+   "owner": "Yoda Rivensky"
  },
  {
    "name": "Event-4",
    "description": "Perfect for people who never swam",
-   "date": "2019-08-28T10:26:37.438199+03:00",
+   "date": "2019-08-29T16:58:02.978696+03:00",
    "cover_url": "",
-   "location": "Rivne, Ukraine"
+   "location": "Rivne, Ukraine",
+   "categories": "software",
+   "owner": "Aliya Workman"
  },
  {
    "name": "Event-3",
    "description": "Perfect for people who never swam",
-   "date": "2019-08-28T10:26:37.437632+03:00",
+   "date": "2019-08-29T16:58:02.978144+03:00",
    "cover_url": "",
-   "location": "Lviv, Ukraine"
+   "location": "Lviv, Ukraine",
+   "categories": "music",
+   "owner": "Gordon Mason"
  },
  {
    "name": "Event-2",
    "description": "Perfect for people who never swam",
-   "date": "2019-08-28T10:26:37.436932+03:00",
+   "date": "2019-08-29T16:58:02.977571+03:00",
    "cover_url": "",
-   "location": "Rivne, Ukraine"
+   "location": "Rivne, Ukraine",
+   "categories": "sport",
+   "owner": "Sonya Alcock"
  }],
         };
 
-        this.toggle = this.toggle.bind(this);
+        
   }
 
-  toggle() {
+  toggle = (event) => {
     this.setState(prevState => ({
-      modal: !prevState.modal
+      modal: !prevState.modal,
+      event
     }));
   }
 
@@ -62,29 +74,18 @@ export default class HomeEvent extends React.Component{
     }
 
     renderEvents(event) {
-        const closeBtn = <button className="close" onClick={this.toggle}>&times;</button>;
+        
         if(!event.cover_url)
             event.cover_url = require("../img/static/event.png");
+
         return (
             <div className="sl-slide" id={`event_${event.id}`} key={event.id}>
                 <img src={event.cover_url} alt={event.name}/>
                 <h3 className="secondary-header">{event.name}</h3>
                 <p className="main-text">{event.description}</p>
                 <div>
-        <Button color="warning" className="btn-yellow" onClick={this.toggle}>{this.props.buttonLabel}Details</Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle} close={closeBtn}><h4 className="secondary-header">{event.name}</h4></ModalHeader>
-          <ModalBody>
-            <img src={event.cover_url} alt={event.name}/>
-            <p className="main-text">{event.description}</p>
-            <p className="main-text">{event.location}</p>
-            <p className="main-text">{event.date}</p>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-      </div>    
+                    <Button color="warning" className="btn-yellow" onClick={() => this.toggle(event)}>{this.props.buttonLabel}Details</Button>
+                </div>    
             </div>
         )
     }
@@ -107,32 +108,50 @@ export default class HomeEvent extends React.Component{
             },
           ]
         };
-
-       console.log(this.state.eventList);   
-
+  
+       const event = this.state.event || this.state.eventsList[0];
+       const closeBtn = <button className="close" onClick={this.toggle}>&times;</button>;
+       const eventDate = event.date;
+       const newEventDate = moment(eventDate).format('MMMM Do YYYY, h:mm:ss a');  
 
         return(
             <div style={{backgroundColor:'#E8E8E8'}} id="home-event">
-            <Container className="home-event">
-            <Row>
-                <Col className="header-block">
-                    <h1>Events near you</h1>
-                    <p className="main-text">People are busy.
-                    So, this UI Kit let’s you customize,
-                    build and deploy your landing page,<br/>
-                    so you can start selling your product
-                    to potential customers.</p>
-                </Col>
-            </Row>
-            <Row>
-            <Col >
-                <Slider {...settings}>
-                   {this.state.eventsList.map( event => this.renderEvents(event) )}
-                </Slider>
-            </Col>
-            </Row>
-            </Container>
+                <Container className="home-event">
+                    <Row>
+                        <Col className="header-block">
+                            <h1>Events near you</h1>
+                            <p className="main-text">People are busy.
+                            So, this UI Kit let’s you customize,
+                            build and deploy your landing page,<br/>
+                            so you can start selling your product
+                            to potential customers.</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col >
+                            <Modal isOpen={this.state.modal} className={this.props.className}>
+                                <ModalHeader toggle={this.toggle} close={closeBtn}><h4 className="secondary-header">{event.name}</h4>
+                                    <p className="main-category">Category:{'  '}{event.categories}</p>
+                                    <p className="main-text-event-modal">{event.description}</p></ModalHeader>
+                                <ModalBody>
+                                   <img src={event.cover_url} alt={event.name}/>
+                                   <p className="main-text">{event.location}</p>
+                                   <p className="main-text">{newEventDate}</p>
+                                   <p className="main-text">Event organizer:{'  '}{event.owner}</p>
+                                </ModalBody>
+                                <ModalFooter>
+                                    <Button className={`btn-join ${isAuthenticated("show")}`} color="warning" onClick={this.toggle}>Join</Button>{' '}
+                                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                                </ModalFooter>
+                            </Modal>  
+                            <Slider {...settings}>
+                               {this.state.eventsList.map( event => this.renderEvents(event) )}
+                            </Slider>   
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
 };
+
