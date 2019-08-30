0ec62ef056d3b1aba73bcd02a7327270ace7ce4a
marina1408 <marina1408@ukr.net>
2019-08-30 20:58:32 +0300

    Added list of events, list of courses for view on homePage

--- frontend/src/Components/HomeEvent.jsx
+++ frontend/src/Components/HomeEvent.jsx
@@ -15,7 +15,42 @@ export default class HomeEvent extends React.Component{
 
         this.state = {
             modal: false,
-            eventsList: [],
+            eventsList: [ {
+   "name": "Event-5",
+   "description": "Perfect for people who never swam",
+   "date": "2019-08-29T16:58:02.979209+03:00",
+   "cover_url": "",
+   "location": "Rivne, Ukraine",
+   "categories": "sport",
+   "owner": "Yoda Rivensky"
+ },
+ {
+   "name": "Event-4",
+   "description": "Perfect for people who never swam",
+   "date": "2019-08-29T16:58:02.978696+03:00",
+   "cover_url": "",
+   "location": "Rivne, Ukraine",
+   "categories": "software",
+   "owner": "Aliya Workman"
+ },
+ {
+   "name": "Event-3",
+   "description": "Perfect for people who never swam",
+   "date": "2019-08-29T16:58:02.978144+03:00",
+   "cover_url": "",
+   "location": "Lviv, Ukraine",
+   "categories": "music",
+   "owner": "Gordon Mason"
+ },
+ {
+   "name": "Event-2",
+   "description": "Perfect for people who never swam",
+   "date": "2019-08-29T16:58:02.977571+03:00",
+   "cover_url": "",
+   "location": "Rivne, Ukraine",
+   "categories": "sport",
+   "owner": "Sonya Alcock"
+ }],
         };    
   }
 
