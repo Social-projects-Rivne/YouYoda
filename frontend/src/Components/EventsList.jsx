390bef48b11f0524e20b89cd0b89d414b54ad7e6
marina1408 <marina1408@ukr.net>
2019-09-02 12:37:27 +0300

    Added the possibility to download the event list from back

--- frontend/src/Components/EventsList.jsx
+++ frontend/src/Components/EventsList.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
+import moment from 'moment';
 
-import { Container, Row, Col, InputGroup, InputGroupText, InputGroupAddon, Input, Button, Card, CardTitle, CardText, CardHeader, CardFooter, CardBody, Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import { axiosGet } from '../api/axiosGet';
+import { Container, Row, Col, InputGroup, InputGroupText, InputGroupAddon, Input, Button, Card, CardTitle, CardText, CardHeader, CardFooter, CardBody } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 export default class EventsList extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            eventsList: [],
+        };    
+    }
+
+    async componentWillMount() {
+        let path = 'events/top'
+        let listEvents = await axiosGet(path);
+        this.setState({
+                eventsList: listEvents,
+                });
+    }
+
+    renderEvents(event) {
+        if(!event.cover_url)
+            event.cover_url = require("../img/static/event.png");
+        const eventDate = event.date;
+        const newEventDate = moment(eventDate).format('MMMM Do YYYY, h:mm:ss a');
+        return (
+       
+           <Col xs="12" sm="6" md="3">
+        <Card className="event-card">
+        <div className="event-header">
+        <CardHeader>
+          <Row>
+          <Col>{newEventDate}</Col>
+          </Row>
+        </CardHeader>
+        </div>
+        <div className="event-body">
+        <CardBody>
+          <CardTitle className="event-card-header">{event.name}</CardTitle>
+          <CardText>
+          <p>Category:{'  '}{event.categories}</p>
+          <div>
+          <Row>
+          <Col md="1">
+          <FontAwesomeIcon icon={['fas', 'map-marker-alt']}/>
+          </Col>
+          <Col md="6"><p>{event.location}</p></Col></Row>
+          </div>
+          </CardText>
+        </CardBody>
+        </div>
+         <CardFooter>
+         <img width="100%" src={event.cover_url} alt={event.name}/>
+         </CardFooter>
+      </Card>
+      </Col>
+    
+        )
+    }
+
+
     render(){
         return(
         	<div>
@@ -28,9 +87,6 @@ export default class EventsList extends React.Component{
                            <option>4</option>
                            <option>5</option>
                          </Input>
-                         <InputGroupText> 
-                         <FontAwesomeIcon icon={['fas', 'map-marker-alt']}/>
-                         </InputGroupText>
                        </InputGroupAddon>              
                      </InputGroup>
                      </Col>
@@ -48,86 +104,13 @@ export default class EventsList extends React.Component{
               <h1 className="title-events">Events</h1>
               </Col>
               </Row>
-
+                  
                <Row>
-      <Col xs="12" sm="6" md="4">
-        <Card className="event-card">
-        <div className="event-header">
-        <CardHeader>
-          <Row>
-          <Col>1 September</Col>
-          <Col style={{textAlign: "right"}}>18:00</Col>
-          </Row>
-        </CardHeader>
-        </div>
-        <div className="event-body">
-        <CardBody>
-          <CardTitle>Solo concert of Oleh Vynyk</CardTitle>
-          <CardText>
-          <p>$ 2</p>
-          <div>
-          <Row>
-          <Col md="1">
-          <FontAwesomeIcon icon={['fas', 'map-marker-alt']}/>
-          </Col>
-          <Col md="6"><p>Rivne, Stadium Avangard</p></Col></Row>
-          </div>
-          </CardText>
-         
-
-        </CardBody>
-        </div>
-         <CardFooter>
-         <img width="100%" src={require('../img/static/event.png')} alt="Card image event" />
-         </CardFooter>
-      </Card>
-      </Col>
-     
+      {this.state.eventsList.map( event => this.renderEvents(event) )}
       
-
     </Row> 
 
-    <div className="pagination"> 
-    <Pagination size="lg" aria-label="Page navigation example">
-      <PaginationItem disabled>
-          <PaginationLink first href="#" />
-        </PaginationItem>
-        <PaginationItem disabled>
-          <PaginationLink previous href="#" />
-        </PaginationItem>
-        <PaginationItem active>
-          <PaginationLink href="#">
-            1
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">
-            2
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">
-            3
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">
-            4
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">
-            5
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink next href="#" />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink last href="#" />
-        </PaginationItem>
-      </Pagination>
-      </div>
+   
 
         	  </Container>
         	</div>       
