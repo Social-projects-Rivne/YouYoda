22bf6cc2a19128af6c5463336ba01acd9e135b45
marina1408 <marina1408@ukr.net>
2019-09-05 10:19:01 +0300

    Created form for sort courses

--- frontend/src/Components/SearchingCourses.jsx
+++ frontend/src/Components/SearchingCourses.jsx
@@ -76,35 +76,37 @@ export default class SearchingCourses extends React.Component{
             <div id="page-wrap">
                 <Router>
                     <Container>
-                        <div className="search-input-group">
-                            <Row>
-                                <Col xs="12" md={{ size: 7, offset: 1 }}>
-                                    <InputGroup className="search-input">
-                                        <InputGroupAddon addonType="prepend">
-                                            <InputGroupText>
-                                                <FontAwesomeIcon icon="search"/>
-                                            </InputGroupText>
-                                        </InputGroupAddon>
-                                        <Input placeholder="Search by trainer, events, tag" />
-                                        <InputGroupAddon addonType="append">
-                                            <Input type="select" name="select" id="exampleSelect">
-                                                <option value="" selected>Category</option>
-                                                <option>1</option>
-                                                <option>2</option>
-                                                <option>3</option>
-                                                <option>4</option>
-                                                <option>5</option>
-                                            </Input>
-                                        </InputGroupAddon>              
-                                    </InputGroup>
-                                </Col>
-                                <Col xs="8" md="4">
+                        <Row>
+                            <Col>
+                                <h1 className="title-events">Courses</h1>
+                            </Col>
+                        </Row>
+                        <Row className="search-input-group">
+                            <Col>
+                                <InputGroup className="search-input">
+                                    <InputGroupAddon addonType="prepend">
+                                        <InputGroupText className="search-input-icon">
+                                            <FontAwesomeIcon icon="search"/>
+                                        </InputGroupText>
+                                    </InputGroupAddon>
+                                        {/*<Input placeholder="Search by trainer, events, tag" />*/}
+                                    <InputGroupAddon addonType="append" className="search-input-select">
+                                        <Input type="select" name="select" id="search-input-select">
+                                            <option value="" selected>Sort by</option>
+                                            <option>Duration</option>
+                                            <option>Start date</option>
+                                            <option>Cost</option>
+                                            <option>Rate</option>
+                                        </Input>
+                                    </InputGroupAddon>              
+                                </InputGroup>
+                            </Col>
+                                {/*<Col xs="8" md="4">
                                     <Button color="warning" className="btn-search-events">
                                         <span className="btn-search-events-title">SEARCH</span>
                                     </Button>  
-                                </Col>
-                            </Row> 
-                        </div>
+                                </Col>*/}
+                        </Row> 
                         <Route
                             path='/courses/search:page'
                             render={() => <Cours coursesList={this.state.coursesList}/>}
@@ -117,15 +119,15 @@ export default class SearchingCourses extends React.Component{
                         <Row>
                             <Col>
                                 <div className="content_detail__pagination cdp" actpage="1">
-                                     <a href="#!-1"
+                                    <a href="#!-1"
                                         className="cdp_i"
                                         onClick={(e) => this.changePrevNext(-1)}
                                         >Prev</a>
                                         {PAGES}
-                                      <a href="#!+1"
+                                    <a href="#!+1"
                                         className="cdp_i"
                                         onClick={(e) => this.changePrevNext(1)}
-                                      >Next</a>
+                                        >Next</a>
                                 </div>
                             </Col>
                         </Row>
