b6bf81b012a32f649ebde8738eff29180ebbc96f
OleksandrHavrylchyk <aleksandr.havrylchyk@gmail.com>
2019-09-10 15:09:23 +0300

    deleted commentaries

--- backend/appsrc/serializers/user_courses_serializer.py
+++ backend/appsrc/serializers/user_courses_serializer.py
@@ -4,28 +4,22 @@ from ..models import CoursesSubscribers, Courses
 
 
 class SubscribedSerializer(serializers.ModelSerializer):
+
+
     class Meta:
+
         model = CoursesSubscribers
         fields = ('completed','feedback','rate',)
-		# fields = "__all__"
+
 
 class UserCoursesSerializer(serializers.ModelSerializer):
 
 	subscribed = SubscribedSerializer(source="subscribed_course", many="True")
-	# course_id = serializers.StringRelatedField()
-	# participant_id = serializers.StringRelatedField()
+    owner = serializers.StringRelatedField()
+	categories = serializers.StringRelatedField()
+
 	class Meta:
 
 		 model = Courses
-		 fields = ("description", "coursename", "subscribed")
-		 # fields = "__all__"
 
-# class UserCoursesSerializer(serializers.Serializer):
-#
-#
-#     id = serializers.IntegerField(read_only=True)
-#     completed = serializers.BooleanField()
-#     feedback = serializers.CharField()
-#     rate = serializers.IntegerField()
-#     course_id = serializers.CharField()
-#     participant_id = serializers.CharField()
+		 exclude = ('id',)
--- backend/appsrc/views/view_profile.py
+++ backend/appsrc/views/view_profile.py
@@ -8,33 +8,28 @@ from rest_framework.permissions import IsAuthenticated
 from ..models import YouYodaUser, CoursesSubscribers, Courses
 from ..serializers.profile_view_serializer import ProfileViewSerializer
 from ..serializers.user_courses_serializer import UserCoursesSerializer
-from ..serializers.courses_serializer import CoursesTopSerializator
 
 
 class ViewProfile(APIView):
     """Takes data from ProfileViewSerializer for view user profile."""
 
     permission_classes = [permissions.IsAuthenticated,]
-    # permission_classes = [permissions.AllowAny,]
 
     def get(self, request):
         """Receives and transmits user profile data"""
         user = YouYodaUser.objects.get(auth_token=request.headers['Authorization'].replace('Token ', ''))
-        # subscribed_courses = CoursesSubscribers.objects.filter(participant_id=user.id)
+        serializer = ProfileViewSerializer(user)
+        return Response(serializer.data)
+
+
+class ViewCoursesProfile(APIView):
+    """Takes data from UserCoursesSerializer for view user courses."""
+
+    permission_classes = [permissions.IsAuthenticated,]
+
+    def get(self, request):
+        """Receives and transmits user courses data"""
+        user = YouYodaUser.objects.get(auth_token=request.headers['Authorization'].replace('Token ', ''))
         user_courses = Courses.objects.filter(subscribed_course__participant_id=user.id)
         courses_serializer = UserCoursesSerializer(user_courses, many=True)
-        # courses = serializers.serialize('json', CoursesSubscribers.objects.filter(participant_id=user.id), fields=('feedback','rate', 'course_id'))
-        # import pdb; pdb.set_trace()
-        # serializer = ProfileViewSerializer(user)
-        # detail_courses=[]
-        # for dict in courses_serializer.data:
-        #     for detail, value in dict.items():
-        #         if detail == 'course_id':
-        #             for s in value:
-        #                 if s.isdigit():
-        #                     detail_courses.append(Courses.objects.get(id=int(s)))
-        # detail_serializer = CoursesTopSerializator(detail_courses, many=True)
-        response=courses_serializer.data
-        # response['courses'] = courses
-        # response['details'] = detail_serializer.data
-        return Response(response)
+        return Response(courses_serializer.data)
