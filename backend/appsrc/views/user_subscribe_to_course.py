d5faeb5a5e8dec9b1a900f4f1066a97687c63599
marina1408 <marina1408@ukr.net>
2019-09-20 16:59:56 +0300

    Updated views user_subscribe_to_course and user_subscribe_to_event for GET API method

--- backend/appsrc/views/user_subscribe_to_course.py
+++ backend/appsrc/views/user_subscribe_to_course.py
@@ -3,16 +3,16 @@ from rest_framework.response import Response
 from rest_framework import permissions, status
 
 from ..models import CoursesSubscribers, YouYodaUser, Courses
-from ..serializers.courses_subscribers_serializer import CoursesSubscribersSerializator
+from ..serializers.courses_subscribers_serializer import CoursesSubscribersPostSerializator, CoursesSubscribersGetSerializator
 
 
 class UserSubscribeToCourse(APIView):
-    """Takes data from CoursesSubscribersSerializator for add user to course"""
+    """Takes data from CoursesSubscribersPostSerializator for add user to course"""
 
     permission_classes = [permissions.IsAuthenticated,]
 
     def post(self, request):
-        """Push user, course to db with CoursesSubscribersSerializator"""
+        """Push user, course to db with CoursesSubscribersPostSerializator"""
         data_course=request.data
         auth_token = request.headers['Authorization'].replace('Token ', '')
         user = YouYodaUser.objects.get(auth_token=auth_token)
@@ -27,9 +27,18 @@ class UserSubscribeToCourse(APIView):
             msg = "You have already subscribed to this course!"
             return Response(msg, status=status.HTTP_208_ALREADY_REPORTED)
         else:
-            serializer = CoursesSubscribersSerializator(data=data_course)
+            serializer = CoursesSubscribersPostSerializator(data=data_course)
             if serializer.is_valid():
                 course_add = serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        """Receives and transmits user course data"""
+        auth_token = request.headers['Authorization'].replace('Token ', '')
+        user = YouYodaUser.objects.get(auth_token=auth_token)
+        courses = CoursesSubscribers.objects.filter(participant_id = user.id)
+        serializer = CoursesSubscribersGetSerializator(courses, many=True)
+        return Response(serializer.data)
+
--- backend/appsrc/views/user_subscribe_to_event.py
+++ backend/appsrc/views/user_subscribe_to_event.py
@@ -3,16 +3,16 @@ from rest_framework.response import Response
 from rest_framework import permissions, status
 
 from ..models import EventsSubscribers, YouYodaUser, Events
-from ..serializers.events_subscribers_serializer import EventsSubscribersSerializator
+from ..serializers.events_subscribers_serializer import EventsSubscribersPostSerializator, EventsSubscribersGetSerializator
 
 
 class UserSubscribeToEvent(APIView):
-    """Takes data from EventsSubscribersSerializator for add user to event"""
+    """Takes data from EventsSubscribersPostSerializator for add user to event"""
 
     permission_classes = [permissions.IsAuthenticated,]
 
     def post(self, request):
-        """Push user, event to db with EventsSubscribersSerializator"""
+        """Push user, event to db with EventsSubscribersPostSerializator"""
         data_event=request.data
         auth_token = request.headers['Authorization'].replace('Token ', '')
         user = YouYodaUser.objects.get(auth_token=auth_token)
@@ -27,9 +27,19 @@ class UserSubscribeToEvent(APIView):
             msg = "You have already subscribed to this event!"
             return Response(msg, status=status.HTTP_208_ALREADY_REPORTED)
         else:
-            serializer = EventsSubscribersSerializator(data=data_event)
+            serializer = EventsSubscribersPostSerializator(data=data_event)
             if serializer.is_valid():
                 event_add = serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        """Receives and transmits user event data"""
+        auth_token = request.headers['Authorization'].replace('Token ', '')
+        user = YouYodaUser.objects.get(auth_token=auth_token)
+        events = EventsSubscribers.objects.filter(participant_id = user.id)
+        serializer = EventsSubscribersGetSerializator(events, many=True)
+        return Response(serializer.data)
+
+        
