f2535966074259d7e8931852fa5eab7755e7707e
OleksandrHavrylchyk <sashagavrulchuk@gmail.com>
2019-10-04 16:16:10 +0300

    Fixed syntax error in sh file

--- backend/migrate.sh
+++ backend/migrate.sh
@@ -2,4 +2,5 @@
 python manage.py migrate
 
 python manage.py shell -c \
-	"from django.contrib.sites.models import Site; Site.objects.filter(domain='example.com').update(domain='localhost:3001', name='YouYoda')"
+	"from django.contrib.sites.models import Site;
+	Site.objects.filter(domain='example.com').update(domain='localhost:3001', name='YouYoda')"
