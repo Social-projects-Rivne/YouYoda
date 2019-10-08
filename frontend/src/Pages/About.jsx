917a9d9a9855401e7bcb41e0b4b973a759d7354d
Hryhorii Kucherchuk <dragonx@meta.ua>
2019-10-08 17:52:56 +0300

    Finished all UI except description

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
 import { Container, Row, Col } from 'reactstrap';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import '../style/about.scss';
+
+import LoginForm from '../Components/LoginForm';
+import Registration from '../Components/Registration';
 
 
 export default class About extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpenL: false,
+            isOpen: false
+        };
+    }
+    handleClickReg = () => {
+        this.setState(prevState => ({
+            isOpen: !prevState.isOpen
+        }));
+    }
+    handleClickLogin = () => {
+        this.setState(prevState => ({
+            isOpenL: !prevState.isOpenL
+        }));
+    }
+
     render() {
         return (
             <>
@@ -14,12 +37,14 @@ export default class About extends React.Component {
                     <div className="info">
                         <Container >
                             <p className="info-text">
-                                YOUYODA: EPISODE I<br /><br />
-                                A long time ago in a galaxy far, far away...<br /><br />
-                                Team of python/web-ui developers decided to make an incredible web-application for self developing, studying and attending events<br /><br />
-                                Our Mission Founded in 2019<br />YouYoda’s mission is to give people the power to build community and bring the world closer together.<br /><br />
-                                People use our products to stay connected with friends and family, to discover what’s going on in the world, and to share and express what matters to them.
-                                </p>
+                                YOUYODA: EPISODE I<br />
+                                A long time ago in a galaxy far, far away...<br />
+                                Team of python/web-ui developers decided to make an incredible web-application for self developing, studying and attending events<br />
+                                Our Mission Founded in 2019<br />
+                                YouYoda’s mission is to give people the power to build community and bring the world closer together.<br />
+                                People use our products to stay connected with friends and family, to discover what’s going on in the world, and to share and express what matters to them.<br />
+                                Our mission is what drives us to do everything possible to expand human potential. We do that by creating groundbreaking sport innovations, by making our products more sustainably, by building a creative and diverse global team and by making a positive impact in communities where we live and work.
+                            </p>
                         </Container>
                     </div>
                 </div>
@@ -27,13 +52,14 @@ export default class About extends React.Component {
 
                 <Container>
                     <Row className="team">
+                        <h1 className="team-head">DEVELOPED BY</h1>
                         <Col className="column-team" xl="4" md="6">
                             <img src={require("../img/static/avatar.png")} alt="ico" />
                             <div className="circle">
                                 <div className="circle-text">
-                                    <p>Hi, my name is Helen. I love programming, music and cakes.<br/>
-                                    I code using Python, PHP, JavaScript, HTML, CSS.<br/>
-                                    I'm searching harmony in the life, doing yoga and I need to study every day as web-developer. My experience contains about 8 years of creating web-sites and internet stores using different CMS APIs, PHP and JS.<br/></p>
+                                    <p>Hi, my name is Helen. I love programming, music and cakes.<br />
+                                        I code using Python, PHP, JavaScript, HTML, CSS.<br />
+                                        I'm searching harmony in the life, doing yoga and I need to study every day as web-developer. My experience contains about 8 years of creating web-sites and internet stores using different CMS APIs, PHP and JS.<br /></p>
                                     <ul>
                                         <h5>Worked on:</h5>
                                         <li>Forgot password system </li>
@@ -77,18 +103,18 @@ export default class About extends React.Component {
                         <Col className="column-team" xl="4" md="6">
                             <img src={require("../img/static/avatar.png")} alt="ico" />
                             <div className="circle">
-                            <div className="circle-text">
-                                <p>Hi, I'm Roman. I'm fond of snowboarding, powerlifting, cooking and hiking.<br/>
-                                I'm coding using Python, JS, React, Css<br/></p>
-                                <ul>
-                                    <h5>Worked on:</h5>
-                                    <li>UI part for fill/edit profile page</li>
-                                    <li>UI part for changing user role from "User" to "Trainer"</li>
-                                    <li>UI part for organizing events</li>
-                                    <li>Implemented method for organizing, managing events</li>
-                                    <li>Implemented method for saving user photos to the content folder and viewing it on the profile page</li>
-                                </ul>
-                            </div>
+                                <div className="circle-text">
+                                    <p>Hi, I'm Roman. I'm fond of snowboarding, powerlifting, cooking and hiking.<br />
+                                        I'm coding using Python, JS, React, Css<br /></p>
+                                    <ul>
+                                        <h5>Worked on:</h5>
+                                        <li>UI part for fill/edit profile page</li>
+                                        <li>UI part for changing user role from "User" to "Trainer"</li>
+                                        <li>UI part for organizing events</li>
+                                        <li>Implemented method for organizing, managing events</li>
+                                        <li>Implemented method for saving user photos to the content folder and viewing it on the profile page</li>
+                                    </ul>
+                                </div>
                             </div>
                             <h1>RUSYN ROMAN</h1>
                             <h3>Software Engineer</h3>
@@ -100,31 +126,43 @@ export default class About extends React.Component {
                 <div className="possibilities">
                     <Container>
                         <Row>
-                            <h1>Afterall you can:</h1>
+                            <LoginForm handleClickLogin={this.handleClickLogin} handleClickReg={this.handleClickReg} isOpenL={this.state.isOpenL} />
+                            <Registration handleClickReg={this.handleClickReg} handleClickLogin={this.handleClickLogin} isOpen={this.state.isOpen} />
+                            <h1>After all you can:</h1>
                             <h3>View our home page, look for public events, courses and trainers</h3>
                             <Col className="column-possibilities" xl="6" md="12">
-                                <h4>Sign up<Button className='btn-sign' color="warning">Become jedi</Button></h4>
+                                <h4>Sign up<Button onClick={this.handleClickReg} className='btn-sign' color="warning">Become jedi</Button></h4>
                             </Col>
                             <Col className="column-possibilities" xl="6" md="12">
-                                <h4>Sign in<Button className='btn-sign' color="warning">Feel the force</Button></h4>
+                                <h4>Sign in<Button onClick={this.handleClickLogin} className='btn-sign' color="warning">Feel the force</Button></h4>
                             </Col>
                             <h3>When you signed in, join events, courses</h3>
                             <Col className="column-possibilities" xl="6" md="12">
-                                <h4>Go to events<Button className='btn-sign' color="warning">Meet up with Yoda</Button></h4>
+                                <h4>Go to events<Link to="/events/search">
+                                    <Button onClick="" className='btn-sign' color="warning">Meet up with Yoda</Button>
+                                </Link></h4>
                             </Col>
                             <Col className="column-possibilities" xl="6" md="12">
-                                <h4>Go to courses<Button className='btn-sign' color="warning">Learn how to control your force</Button></h4>
+                                <h4>Go to courses<Link to="/courses/search">
+                                    <Button className='btn-sign' color="warning">Learn how to control your force</Button>
+                                </Link></h4>
                             </Col>
                             <h3>Or become trainer to create your own courses/events</h3>
                             <Col className="column-possibilities" xl="6" md="12">
-                                <h4>Become trainer<Button className='btn-sign' color="warning">Become jedi instructor</Button></h4>
+                                <h4>Become trainer<Link to="/editprofile">
+                                    <Button className='btn-sign' color="warning">Become jedi instructor</Button>
+                                </Link></h4>
                             </Col>
                             <Col className="column-possibilities" xl="6" md="12">
-                                <h4>Go to trainer page<Button className='btn-sign' color="warning">Find your jedi instructor</Button></h4>
+                                <h4>Go to trainer page<Link to="/trainers">
+                                    <Button className='btn-sign' color="warning">Find your jedi instructor</Button>
+                                </Link></h4>
                             </Col>
                             <h3>Also you can develop yourself as a plain user by PDP(Personal Development Plan) system</h3>
                             <Col className="column-possibilities">
-                                <h4>Go to your PDP page<Button className='btn-sign' color="warning">Learn jedi skills on your own</Button></h4>
+                                <h4>Go to your PDP page<Link to="/profile">
+                                    <Button className='btn-sign' color="warning">Learn jedi skills on your own</Button>
+                                </Link></h4>
                             </Col>
                         </Row>
                     </Container>
